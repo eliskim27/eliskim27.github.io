868168a6ae8fcc784757aba5cb59de51a5100851
eliskim27 <eliskim27@gmail.com>
2020-05-12 22:39:42 -0400

    update social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,12 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-                
+                <a href="https://github.com/eliskim27" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="http://www.linkedin.com/in/eli-kim-97b719bb" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://medium.com/@eliskim27" target="_blank"><i class="fab fa-medium-m"></i></a>
+                {/* <a href="https://codepen.io/" target="_blank"><i class="fab fa-codepen"></i></a> */}
+                {/* <a href="https://Instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a> */}
+                {/* <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a> */}
             </div>
         )
     }
