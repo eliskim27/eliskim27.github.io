1bb1fdf72763824975d76a78ac550366fce04cd0
eliskim27 <eliskim27@gmail.com>
2020-05-19 12:37:43 -0400

    add router: home

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import
     Route,
   } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './contents/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
       </div>
     </Router>
   )
