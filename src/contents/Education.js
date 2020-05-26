d7f30d3bbd45d0a771dccf3417d0ad08ec272b96
eliskim27 <eliskim27@gmail.com>
2020-05-26 09:49:15 -0400

    adjust education cards

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -6,8 +6,16 @@ class Education extends Component {
         return (
             <div className="condiv">
                 <h1 className="subtopic">My Education</h1>
-                <Widecard title="M.Tech Software Engineering" where="VIT University" from="July 2017" to="Present"/>
-                <Widecard title="SSLC | HSC" where="Islamiah Higher Secondary School" from="2011" to="2017"/>
+                <Widecard 
+                    title="Fullstack Software Engineering" 
+                    where="Flatiron School" 
+                    from="sometime" 
+                    to="sometime"/>
+                <Widecard 
+                    title="BA in Human Resources Management" 
+                    where="Rutgers University" 
+                    from="sometime" 
+                    to="sometime"/>
             </div>      
         )
     }
