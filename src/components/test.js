072a40ea58b06cfc113f160fb6d67a6c1a46ed36
eliskim27 <eliskim27@gmail.com>
2020-06-02 14:57:42 -0400

    confirm navtiem

--- src/components/test.js
+++ src/components/test.js
@@ -1,36 +1,16 @@
 // import React, { Component } from 'react';
-// import Navitem from './Navitem';
+// import {
+//     Link
+// } from "react-router-dom";
 
-// class Navbar extends Component {
-//     constructor(props)
-//     {
-//         super(props);
-//         this.state={
-//             'NavItemActive':''
-//         }
-//     }
-//     activeitem=(x)=>
-//     {
-//         if(this.state.NavItemActive.length>0){
-//             document.getElementById(this.state.NavItemActive).classList.remove('active');
-//         }
-//         this.setState({'NavItemActive':x},()=>{
-//             document.getElementById(this.state.NavItemActive).classList.add('active');
-//         });
-//     };
+// class Navitem extends Component {
 //     render() {
-//         return (
-//             <nav>
-//             <ul>
-//             <Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
-//             <Navitem item="About" tolink="/about"  activec={this.activeitem}></Navitem>
-//             <Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem>
-//             <Navitem item="Skills" tolink="/skills"  activec={this.activeitem}></Navitem>
-//             <Navitem item="Contact" tolink="/contact"  activec={this.activeitem}></Navitem>
-//             </ul>
-//             </nav>
-//         )
-//     }
-// }
-    
-//     export default Navbar
+//                 return (
+//                     <li id={this.props.item}>
+//                     <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
+//                     </li>
+//                     )
+//             }
+//         }
+        
+//         export default Navitem
