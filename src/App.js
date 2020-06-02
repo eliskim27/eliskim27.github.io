c9ba320dd87c2df88afd8ec8a221d0e096c87b07
eliskim27 <eliskim27@gmail.com>
2020-06-02 14:30:26 -0400

    route for contact.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './contents/Home';
 import About from './contents/About';
 import Education from './contents/Education';
 import Skills from './contents/Skills';
+import Contact from './contents/Contact';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/about">       <About />       </Route>
         <Route path="/education">   <Education />   </Route>
         <Route path="/skills">      <Skills />      </Route>
+        <Route path="/contact">     <Contact />     </Route>
       </div>
     </Router>
   )
