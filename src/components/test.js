41a01568e6c2b38b01c68c3750b443b6095df398
eliskim27 <eliskim27@gmail.com>
2020-06-02 15:01:47 -0400

    confirm social

--- src/components/test.js
+++ src/components/test.js
@@ -1,16 +1,18 @@
-// import React, { Component } from 'react';
-// import {
-//     Link
-// } from "react-router-dom";
+import React, { Component } from 'react';
 
-// class Navitem extends Component {
-//     render() {
-//                 return (
-//                     <li id={this.props.item}>
-//                     <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
-//                     </li>
-//                     )
-//             }
-//         }
-        
-//         export default Navitem
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://codepen.io/naafi" target="_blank"><i class="fab fa-codepen"></i></a>
+            <a href="https://github.com/naaficodes" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://Instagram.com/iam_naafi" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/abdul.w.naafi" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/naafi" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@iam_naafi" target="_blank"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default 
