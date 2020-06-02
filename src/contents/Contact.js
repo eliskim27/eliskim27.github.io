7cc8deb3ab16dac35f5753acbd0069ff5640e026
eliskim27 <eliskim27@gmail.com>
2020-06-02 14:14:40 -0400

    update contact info

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -6,8 +6,8 @@ class Contact extends Component {
         return (
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
-                <h3>Email  :   naafi96@gmail.com</h3>
-                <h3>Instagram   :   @iam_naafi</h3>
+                <h3>Email  :   fill in</h3>
+                <h3>Instagram   :   fill in</h3>
 
                 <Social />
             </div>
