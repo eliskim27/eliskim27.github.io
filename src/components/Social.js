e99cfb852fcaeba77cff4aad8fbd24e66f9d52c5
eliskim27 <eliskim27@gmail.com>
2020-06-22 21:04:32 -0400

    fixed social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import gitIcon from '../img/icons8-github-50.png';
+import linkedinIcon from '../img/icons8-linkedin-50-2.png';
+import mediumIcon from '../img/icons8-medium-monogram-50.png';
 
 class Social extends Component {
     render() {
         return (
             <div className="social">
-                <a href="https://github.com/eliskim27" 
-                    target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-                <a href="https://Linkedin.com/in/eli-kim-97b719bb" 
-                    target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
-                <a href="https://medium.com/@eliskim27" 
-                    target="_blank" rel="noopener noreferrer"><i className="fab fa-medium-m"></i></a>
+                <a href="https://github.com/eliskim27" target="_blank" rel="noopener noreferrer">
+                    <img src={gitIcon} alt="github"/>
+                    <i className="fab fa-github"></i>
+                </a>
+                <a href="https://Linkedin.com/in/eli-kim-97b719bb" target="_blank" rel="noopener noreferrer">
+                    <img src={linkedinIcon} alt="github"/>
+                    <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a href="https://medium.com/@eliskim27" target="_blank" rel="noopener noreferrer">
+                    <img src={mediumIcon} alt="github"/>
+                    <i className="fab fa-medium-m"></i>
+                </a>
                 
                 {/* <a href="https://codepen.io/" target="_blank"><i className="fab fa-codepen"></i></a> */}
                 {/* <a href="https://Instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a> */}
@@ -20,3 +29,9 @@ class Social extends Component {
 }
 
 export default Social
+
+// medium
+// https://imgur.com/Cr9Mnrx
+// github
+// https://imgur.com/Xdi9gC9
+
