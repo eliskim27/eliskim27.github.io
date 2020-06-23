fa575445792285a866f5fabb8db4d12d895bbc04
eliskim27 <eliskim27@gmail.com>
2020-06-22 22:27:21 -0400

    adjust typing messages on home.js

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -9,17 +9,32 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
+                <h1 className="typingeffect" >Hi, I'm Eli.</h1>
                 <img 
                     src={profilepic} 
                     className="profilepic"
                     alt="Welcome to Eli's Portfolio!">
                     </img>
-                <ReactTypingEffect 
-                    className="typingeffect" 
-                    text={["Hello, I'm Eli!", "I'm a web developer"]} 
-                    speed={70} 
-                    eraseDelay={500}
-                />
+                <div>
+                    <ReactTypingEffect
+                        className="typingeffect" 
+                        text={["I'm "]} 
+                        speed={70} 
+                        eraseDelay={100000000000}
+                        cursor={String}
+                    />
+                    <ReactTypingEffect 
+                        className="typingeffect" 
+                        text={[
+                            " a software engineer!",
+                            " a progammer!",
+                            " a lifelong learner!",
+                            " eager to meet you!"
+                        ]} 
+                        speed={70} 
+                        eraseDelay={10}
+                    />
+                </div>
                 <Social />
             </div>
         )
