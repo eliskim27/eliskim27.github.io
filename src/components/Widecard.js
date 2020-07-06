75688e2f72c7f99746d7db4bef859290100165ea
eliskim27 <eliskim27@gmail.com>
2020-07-05 20:12:44 -0400

    adjust education format

--- src/components/Widecard.js
+++ src/components/Widecard.js
@@ -6,8 +6,8 @@ class Widecard extends Component {
         return (
             <div className="widecard">
                 <div className="compdet">
-                    <h3>{this.props.title}</h3>
-                    <h4 className="secondtext">{this.props.schoolName}</h4>
+                    <h2>{this.props.title}</h2>
+                    <h4 className="secondtext">{this.props.schoolName} - ({this.props.location})</h4>
                     <h4 className="secondtext">{this.props.to}</h4>
                 </div>
             </div>
--- src/contents/Education.js
+++ src/contents/Education.js
@@ -9,16 +9,19 @@ class Education extends Component {
                 <Widecard 
                     title="Software Engineering Program" 
                     schoolName="Flatiron School" 
+                    location="New York, NY"
                     to="2020"
                 />
                 <Widecard 
                     title="BA in Human Resource Management" 
                     schoolName="Rutgers University" 
+                    location="New Brunswick, NJ"
                     to="2017"
                 />
                 <Widecard 
                     title="31B - Military Police" 
                     schoolName="Army Basic Training / AIT" 
+                    location="Ft. Leonard Wood, MO"
                     to="2013"
                 />
             </div>
