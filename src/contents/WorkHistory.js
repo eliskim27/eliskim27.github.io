430d1a880cf46c8d848317edfc4c27c192495abc
eliskim27 <eliskim27@gmail.com>
2020-07-13 22:35:30 -0400

    create workshitory component

---
+++ src/contents/WorkHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WorkHistory extends Component {
+    render() {
+        return (
+            <div>
+                WorkHistory Component
+            </div>
+        )
+    }
+}
+    
+export default WorkHistory
