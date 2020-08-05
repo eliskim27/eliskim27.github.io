776785fc0bd70f7f7a9a814d19d5bad43f208242
eliskim27 <eliskim27@gmail.com>
2020-08-04 20:52:36 -0400

    remove skills page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,6 +27,7 @@ class NavBar extends Component {
                     <Navitem item="Home"         tolink="/"              activec={this.activeitem}></Navitem>
                     <Navitem item="About"        tolink="/about"         activec={this.activeitem}></Navitem>
                     <Navitem item="Education"    tolink="/education"     activec={this.activeitem}></Navitem>
+                    {/* maybe remove skills. whats the purpose? */}
                     <Navitem item="Skills"       tolink="/skills"        activec={this.activeitem}></Navitem>
                     {/* <Navitem item="Contact"      tolink="/contact"       activec={this.activeitem}></Navitem> */}
                     <Navitem item="Work History" tolink="/workhistory"   activec={this.activeitem}></Navitem>
