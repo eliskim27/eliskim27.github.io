edb5d5ef92911119d0a5199f0d8a84f79adabeb1
eliskim27 <eliskim27@gmail.com>
2020-08-11 20:12:08 -0400

    adjust profile picutre size

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -44,7 +44,6 @@ class Home extends Component {
                         typingDelay={200}
                     />
                 </div>
-                {/* <Social /> */}
             </div>
         )
     }
