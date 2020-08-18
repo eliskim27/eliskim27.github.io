7ef117fd0bde0b704d764ec470a21065c38679ea
eliskim27 <eliskim27@gmail.com>
2020-08-18 10:19:31 -0400

    add samsung work history

--- src/contents/WorkHistory.js
+++ src/contents/WorkHistory.js
@@ -18,6 +18,41 @@ class WorkHistory extends Component {
                         'Launched new markets and programs, advising on strategic marketing and events to include: Seattle, Denver, Chicago, Data Science, UX/UI Design, and veteran initiatives such as GI-Bill implementation and VET-TEC.'
                     ]}
                 />
+                <WorkCard
+                    company='Samsung Electronics America'
+                    title='Inside Sales Representative'
+                    startDate='06/2016'
+                    endDate='11/2018'
+                    description={[
+                        'Assumed responsibilities of Team Lead for Lead Development to include: delegating of daily incoming leads, reporting on Key Performance Indicators, and identifying inefficiencies for training purposes',
+                        'Member of cross-departmental QA Testing Team for newer enhancements in Salesforce ensuring that the enhancements function properly and are optimized for day-to-day use',
+                        'Leveraged internal network of technical product engineers and other resources to help close potential sale'
+                    ]}
+                />
+                <WorkCard
+                    company='Flatiron School'
+                    title='Admissions Lead'
+                    startDate='11/2018'
+                    endDate='11/2019'
+                    description={[
+                        'More than doubled revenue generated in the Washington DC market to become responsible for ~$1.2M quarterly.',
+                        'Managed competing priorities of local campus and HQ, building bridges with local teams and forging stronger and more effective relationships.',
+                        'Designed and implemented more streamlined admissions processes, cohort start management, and overall student experience.',
+                        'Launched new markets and programs, advising on strategic marketing and events to include: Seattle, Denver, Chicago, Data Science, UX/UI Design, and veteran initiatives such as GI-Bill implementation and VET-TEC.'
+                    ]}
+                />
+                <WorkCard
+                    company='Flatiron School'
+                    title='Admissions Lead'
+                    startDate='11/2018'
+                    endDate='11/2019'
+                    description={[
+                        'More than doubled revenue generated in the Washington DC market to become responsible for ~$1.2M quarterly.',
+                        'Managed competing priorities of local campus and HQ, building bridges with local teams and forging stronger and more effective relationships.',
+                        'Designed and implemented more streamlined admissions processes, cohort start management, and overall student experience.',
+                        'Launched new markets and programs, advising on strategic marketing and events to include: Seattle, Denver, Chicago, Data Science, UX/UI Design, and veteran initiatives such as GI-Bill implementation and VET-TEC.'
+                    ]}
+                />
 
 
                 
