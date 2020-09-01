35310f390113235686212846188281612bf1ceb6
eliskim27 <eliskim27@gmail.com>
2020-08-31 21:01:26 -0400

    import resume from ....

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Document } from 'react-pdf'
+import resume from '../img/Eli Kim Resume.pdf'
 
 class Resume extends Component {
     render() {
         return (
             <div className="condiv">
-                <h1 className="subtopic">upload copy of resume here w/ btn to download</h1>
+                <h1 className="subtopic">Resume</h1>
+                <Document
+                    // file="../img/Eli Kim Resume.pdf"
+                >
+
+                </Document>
             </div>
         )
     }
